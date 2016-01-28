6b32d7ab27bcbe681414ed0bcdefdbb0bd3dfa6a
Ashu <ashutshu@ASHUTSHU-IN.in.oracle.com>
2016-01-28 19:16:17 +0530

    New Project

---
+++ DevopsDemo/src/test/java/com/javacodegeeks/SimpleTest.java
@@ -0,0 +1,17 @@
+package test.java.com.javacodegeeks;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import main.java.com.hascode.tutorial.cucumber.login.action.SampleExample;
+
+public class SimpleTest {
+	
+	@Test
+	public void test() {
+		SampleExample example = new SampleExample();
+		example.addInteger(10);
+		example.addInteger(100);
+		Assert.assertEquals(example.getSize(), 2);
+	}
+}
